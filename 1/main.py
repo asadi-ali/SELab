a8cc0838d274a529d3ffbd52e38ebd0bb5f14957
Ali Asadi <aliasadi@cafebazaar.ir>
2021-03-07 17:42:45 +0330

    Add getting operands procedure

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,8 @@
 if __name__ == '__main__':
     while True:
         command = input("Input operator: ")
-
         if command == 'exit':
             exit(0)
+
+        operands = input("Input two operands: ")
+        operands = list(map(int, operands.split(' ')))
