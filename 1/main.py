9cdd95b60369268a750af53ba134254310eb2432
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2021-03-07 17:46:41 +0330

    Implement minus function

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,8 @@
 def minus(operands):
-    pass
+    print("{op1} - {op2} = {res}".format(
+        op1=operands[0],
+        op2=operands[1],
+        res=operands[0] - operands[1]))
 
 
 if __name__ == '__main__':
