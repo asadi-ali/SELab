4f6fa43deb0623ed8894d7a67f4839e66f4c580a
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2021-03-14 19:47:12 +0330

    Add Rectangle set_height test

--- 2/tests.py
+++ 2/tests.py
@@ -7,3 +7,8 @@ class RectangleUnitTest(unittest.TestCase):
     def test_rectangle_area(self):
         rectangle: Rectangle = Rectangle(height=3, width=2)
         self.assertEqual(rectangle.compute_area(), 6)
+
+    def test_rectangle_set_height(self):
+        rectangle: Rectangle = Rectangle(height=3, width=2)
+        rectangle.set_height(5)
+        self.assertEqual(rectangle.get_height(), 5)
