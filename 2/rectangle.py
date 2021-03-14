a9187844fed7c773638a0c3f1fbbb644d8056339
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2021-03-14 19:51:14 +0330

    Implement set_height and get_height methods in order to pass the test

--- 2/rectangle.py
+++ 2/rectangle.py
@@ -9,8 +9,8 @@ class Rectangle(Shape):
     def compute_area(self) -> int:
         return self.height * self.width
 
-    def set_height(self, new_height) -> int:
-        pass
+    def set_height(self, new_height) -> None:
+        self.height = new_height
 
     def get_height(self) -> int:
-        pass
+        return self.height
