27d58b3e54efbdbc99ccfa0f587353dfdca1e3b1
Ali Asadi <aliasadi@cafebazaar.ir>
2021-04-15 16:11:42 +0430

    Add sign-in and get-username

--- 4/auth/main.py
+++ 4/auth/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+import jwt
 
 from .db import run_migrations, close_connection, query_db
 
 app = Flask(__name__)
+jwt_secret_key = 'random secret'
 
 
 @app.before_first_request
@@ -30,3 +32,41 @@ def sign_up():
         """, args=(username, password), with_commit=True)
 
     return jsonify({}), 201
+
+
+@app.route('/auth/sign-in', methods=['POST'])
+def sign_in():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    # TODO: Validate this values
+
+    user = query_db('select * from auth_user where username = ? and password = ?',
+                    args=(username, password), one=True)
+    if not user:
+        return jsonify({"error": "You should enter correct username and password."}), 401
+
+    token = jwt.encode(
+        payload={"username": username},
+        key=jwt_secret_key,
+        algorithm="HS256"
+    )
+
+    response = make_response(jsonify({}))
+    response.headers['Authorization'] = token
+    return response
+
+
+@app.route('/auth/get-username', methods=['GET'])
+def get_username():
+    token = request.headers['Authorization']
+
+    data = jwt.decode(
+        jwt=token,
+        key=jwt_secret_key,
+        algorithms=["HS256"]
+    )
+
+    if 'username' not in data:
+        return jsonify({"error": "Authorization header is not valid."}), 401
+
+    return jsonify({'username': data['username']}), 200
